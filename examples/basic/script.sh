0be26eb48bec7be02d4c90c75b74a251a7a9e74b
Sivagiri Visakan <siva.g.visakan@gmail.com>
2021-09-19 21:20:23 +0530

    Change serverless.yml and example script

--- examples/basic/script.sh
+++ examples/basic/script.sh
@@ -6,10 +6,10 @@ migrations_bucket="prod-db-migrations"
 zip_name="migrations.zip"
 
 echo "Zipping migrations"
-cd dbmigration && zip "../$zip_name" * && cd -
+cd dbmigration && zip -r "../$zip_name" * && cd -
 
 # Upload the ZIP to s3
-s3_upload_path="s3://$migrations_bucket/$project"
+s3_upload_path="s3://$migrations_bucket/$project/$zip_name"
 echo "Uploading migration archive to S3: ${s3_upload_path}"
 aws s3 cp "$zip_name" "$s3_upload_path"
 
@@ -18,4 +18,4 @@ echo "Deleting migration archive: $zip_name"
 rm "$zip_name"
 
 ## Invoke s3-migrate lambda
-s3migrate up --lambda "dbMigrationHandler" --bucket "$migrations_bucket" --archive-path "$s3_upload_path/$zip_name" prod
+sls-dbmigrate up --lambda "dbMigrationHandler" --bucket "$migrations_bucket" --archive-path "$s3_upload_path" prod
